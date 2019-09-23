6c59f6fe883b813e8a411cadc8fa3231bc41b317
lanhoter <lanhoter@hotmail.com>
2019-09-23 15:54:38 +0100

    Removed unused files

--- javascript-nodejs/src/send.js
+++ javascript-nodejs/src/send.js
@@ -2,11 +2,11 @@
 
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(error0, connection) {
+amqp.connect('amqp://localhost', function (error0, connection) {
     if (error0) {
         throw error0;
     }
-    connection.createChannel(function(error1, channel) {
+    connection.createChannel(function (error1, channel) {
         if (error1) {
             throw error1;
         }
@@ -21,7 +21,7 @@ amqp.connect('amqp://localhost', function(error0, connection) {
 
         console.log(" [x] Sent %s", msg);
     });
-    setTimeout(function() {
+    setTimeout(function () {
         connection.close();
         process.exit(0);
     }, 500);
